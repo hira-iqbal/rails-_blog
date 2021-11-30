323ff550b13423193eecca868b4c13dd2139d06a
Hira Iqbal <hirach502@gmail.com>
2021-11-30 19:09:15 +0500

    adding role to blog using seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+option = Role.insert_all([
+  { :name => 'admin' },
+  { :name => 'moderator' },
+  { :name => 'editor' },
+  { :name => 'commenter'}])
+
+User.create!([{name: "Ali", surname: "Ammar", email: "admin@gmail.com", password: "12345678", role: "admin"}])
+
